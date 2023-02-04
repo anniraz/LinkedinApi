8641489e93d7dbabcff49178a1e93445fb453aef
anniraz <zarinakudajberdikyzy@gmail.com>
2023-02-05 01:04:50 +0600

    little changes

--- LinkedinDRF/settings.py
+++ LinkedinDRF/settings.py
@@ -16,7 +16,7 @@ SECRET_KEY = 'django-insecure-dea%8ly_d=^a5#&g(^-09^@-v&u3*k13+)#+flw)7d1*tazb^z
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
